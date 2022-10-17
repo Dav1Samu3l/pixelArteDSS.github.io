aabf6d67aa75b74306573c96e5ae11912ecb0210
Dav1Samu3l <davidev.front@gmail.com>
2022-10-17 18:20:20 -0300

    referencias iniciais  declaradas

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit_grid");
+let clearGridButton = document.getElementById("clear_grid");
+let gridWidth = document.getElementById("width_range");
+let gridHeigth = document.getElementById("heidth_range");
+let colorButton = document.getElementById("color_input")
+let eraseBtn = document.getElementById("erase_btn")
+let paintBtn = document.getElementById("paint_btn")
+let widthValue = document.getElementById("width_value")
+let heigthValue = document.getElementById("heigth_value")
